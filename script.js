c93d3ef3504c9e92bde560aa6793a3c78e3732ef
Rijwanul <rijwanulhoque@gmail.com>
2021-01-27 11:55:28 +0600

    quantity decrease function

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var decreaseQuantity = document.getElementById ('decreaseQuantity1');
 decreaseQuantity.addEventListener('click', function(){
+    decreaseFunction ('currentQuantity1');
+});
+
+//Quantity Decrease Function
+function decreaseFunction(quantitySerial) {
     var theQuantity = document.getElementById('currentQuantity1');
     var currentQuantity = theQuantity.value;
     currentQuantityNumber = parseInt(currentQuantity);
     var newQuantity = currentQuantityNumber-1;
     theQuantity.value = newQuantity;
-});
+}
