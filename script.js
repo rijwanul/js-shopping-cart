fbc19cd81e3975158ab2321382964a9ca957672b
Rijwanul <rijwanulhoque@gmail.com>
2021-01-27 12:15:59 +0600

    added condition

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-var decreaseQuantity = document.getElementById ('decreaseQuantity1');
-decreaseQuantity.addEventListener('click', function(){
-    decreaseFunction ('currentQuantity1');
+//Cart item 1 - decrease quantity
+const decreaseQuantity = document.getElementById('decreaseQuantity1');
+decreaseQuantity.addEventListener('click', function () {
+    quantityFunction('currentQuantity1', -1);
+
+    updateProductTotal('currentPrice1', -1);
 });
 
-//Quantity Decrease Function
-function decreaseFunction(quantitySerial) {
-    var theQuantity = document.getElementById('currentQuantity1');
+//Cart item 1 - increase quantity
+const increaseQuantity = document.getElementById('increaseQuantity1');
+increaseQuantity.addEventListener('click', function () {
+    quantityFunction('currentQuantity1', 1);
+
+    updateProductTotal('currentPrice1', 1);
+});
+
+
+//Functions Hub
+//Quantity Function
+function quantityFunction(quantitySerial, plusMinus) {
+    var theQuantity = document.getElementById(quantitySerial);
     var currentQuantity = theQuantity.value;
     currentQuantityNumber = parseInt(currentQuantity);
-    var newQuantity = currentQuantityNumber-1;
-    theQuantity.value = newQuantity;
+    if (currentQuantityNumber > 0) {
+        var newQuantity = currentQuantityNumber + plusMinus;
+        theQuantity.value = newQuantity;
+    }
+}
+
+//change total product price
+function updateProductTotal(thePrice, plusMinus) {
+    const currentPrice = document.getElementById(thePrice);
+    var currentPriceNumber = parseInt(currentPrice.innerText);
+    var newPrice = currentPriceNumber + (1219 * plusMinus);
+    currentPrice.innerText = newPrice;
 }
